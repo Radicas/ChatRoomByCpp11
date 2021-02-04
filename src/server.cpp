00ca932d74838b4d91f5a9fa55a9172a487381b1
Radicas <Z1176814811@gmail.com>
2021-02-05 00:45:58 +0800

    C语言客户端和服务端转化成C++

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +9,7 @@
 #include <sys/select.h>
 #define BUF_SIZE 100
 
-void error_handling(char* message);
-
+void error_handling(const char message[]);
 int main(int argc, char* argv[])
 {
 	int serv_sock, clnt_sock;	//服务端、客户端套接字
@@ -22,19 +22,22 @@ int main(int argc, char* argv[])
 	char buf[BUF_SIZE];
 	if(argc!=2)
 	{
-		printf("Usage: %s", argv[0]);
+		printf("Usage: %s <port>", argv[0]);
 		exit(1);
 	}
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock == -1)
+		error_handling("socket error");
+	
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_adr.sin_port = htons(atoi(argv[1]));
 	
 	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
-		error_handling("bind() error");
+		error_handling("bind error");
 	if(listen(serv_sock, 5)==-1)
-		error_handling("listen() error");
+		error_handling("listen error");
 
 	FD_ZERO(&reads);	//fd_set变量初始化
 	FD_SET(serv_sock, &reads);	//将serv_sock信息注册到reads变量中
@@ -55,10 +58,12 @@ int main(int argc, char* argv[])
 		 *成功返回正整数，失败返回-1，超时返回0
 		 * */
 		if((fd_num = select(fd_max+1, &cpy_reads, 0, 0, &timeout))==-1)	//调用select函数
+		{
+			error_handling("select error");
 			break;
+		}
 		if(fd_num == 0)
 		{
-			fputs("time out \n", stderr);	
 			continue;
 		}
 		for(i=0; i<fd_max+1; i++)
@@ -69,6 +74,8 @@ int main(int argc, char* argv[])
 				{
 					adr_sz = sizeof(clnt_adr);
 					clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+					if(clnt_sock == -1)
+						error_handling("accept error");
 					FD_SET(clnt_sock, &reads);
 					if(fd_max<clnt_sock)
 						fd_max = clnt_sock;
@@ -78,10 +85,10 @@ int main(int argc, char* argv[])
 					str_len = read(i, buf, BUF_SIZE);
 					if(str_len == -1)
 					{
-						error_handling("read() error");
+						error_handling("read error");
 					}else
 					{
-						printf("from client: %s \n", buf);
+						//处理消息
 					}
 					if(str_len == 0)
 					{
@@ -100,9 +107,8 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void error_handling(char* message)
+void error_handling(const char message[])
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
+	std::cout << message << std::endl;
 	exit(1);
 }
