1a403b849de059d630cfb32b52c499576abaaf82
Radicas <Z1176814811@gmail.com>
2021-02-05 13:23:58 +0800

    Qt构建简单GUI面板

--- chatclient/client/dialog.cpp
+++ chatclient/client/dialog.cpp
@@ -1,6 +1,7 @@
 #include "dialog.h"
 #include "ui_dialog.h"
-
+#include <string.h>
+#include <QMessageBox>
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
@@ -13,3 +14,20 @@ Dialog::~Dialog()
     delete ui;
 }
 
+void Dialog::on_clear_in_clicked()
+{
+    ui->ip->clear();
+    ui->port->clear();
+    ui->textBrowser->clear();
+}
+
+void Dialog::on_connect_clicked()
+{
+    QString ip = ui->ip->toPlainText();
+    QString port = ui->port->toPlainText();
+}
+
+void Dialog::on_Send_clicked()
+{
+
+}
--- chatclient/client/dialog.h
+++ chatclient/client/dialog.h
@@ -2,6 +2,7 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Dialog; }
@@ -9,13 +10,21 @@ QT_END_NAMESPACE
 
 class Dialog : public QDialog
 {
-    Q_OBJECT
+    Q_OBJECT    //默认是private的
 
 public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_clear_in_clicked();
+
+    void on_connect_clicked();
+
+    void on_Send_clicked();
+
 private:
     Ui::Dialog *ui;
+
 };
 #endif // DIALOG_H
